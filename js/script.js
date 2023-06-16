22b512abf895feae546ff2b4e88198c7e8ef36c0
masepe95 <marcosepe10@gmail.com>
2023-06-16 18:15:02 +0200

    creapApp added- mount on DOM

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 console.log('JS OK', Vue) 
 
+
+// Estrapolo metodo
+const { createApp } = Vue;
+
+// Inizializzo app Vue
+const app = createApp({
+    data() {
+        return {
+            firstName: 'Marco',
+            lastName: 'Sepe',
+            pic: 'https://picsum.photos/id/9/600/400'
+
+        }
+    }
+})
+
+//Add to DOM
+app.mount('#root');
